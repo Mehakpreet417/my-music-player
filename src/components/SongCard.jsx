6c796409145628488844f28b218385a94be3029a
Mehakpreet Chopra <64402765+Mehakpreet417@users.noreply.github.com>
2025-02-09 17:27:01 +0530

    Update SongCard.jsx

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -2,14 +2,14 @@ import React, { useContext } from "react";
 import SongContext from "../contexts/SongContext";
 
 const SongCard = ({ song }) => {
-  const { setCurrentSong } = useContext(SongContext);
+  const { setCurrentSongIndex } =useContext(SongContext);
 
   return (
     <div
       className="bg-transparent  rounded-lg shadow hover:bg-gray-700 cursor-pointer text-center"
-      onClick={() => setCurrentSong(song)}
+      onClick={() => setCurrentSongIndex(song.id -1)}
     >
-      <img src={song.albumArt} alt={song.title} className="rounded-lg mb-2" />
+      <img src={song.albumArt} alt={song.title} className="rounded-lg mb-2 h-[145px]" />
       <h3 className="text-[#EAF0FF] font-[400] text-[14px] ">{song.title}</h3>
       <p className="font-[400] text-[10px] text-[#A5C0FFB2]">{song.artist}</p>
     </div>
