42b34fc9f5967218d0ec67502a753e13fde56e85
Mehakpreet Chopra <64402765+Mehakpreet417@users.noreply.github.com>
2025-02-09 17:38:06 +0530

    Update SongCard.jsx

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -9,7 +9,7 @@ const SongCard = ({ song }) => {
       className="bg-transparent  rounded-lg shadow hover:bg-gray-700 cursor-pointer text-center"
       onClick={() => setCurrentSongIndex(song.id -1)}
     >
-      <img src={song.albumArt} alt={song.title} className="rounded-lg mb-2 h-[145px]" />
+      <img src={song.albumArt} alt={song.title} className="rounded-lg mb-2" />
       <h3 className="text-[#EAF0FF] font-[400] text-[14px] ">{song.title}</h3>
       <p className="font-[400] text-[10px] text-[#A5C0FFB2]">{song.artist}</p>
     </div>
